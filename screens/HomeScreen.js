8c85242b092ae32b654b24e6d1adb0c17bcd54f1
Lucas Domene <lucas.firmo@gmail.com>
2024-04-30 08:29:19 -0300

    feat: add HomeScreen navigation bar

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,15 +1,41 @@
 import React from 'react';
 
-import { View, Text } from 'react-native';
+import {
+  View,
+  Platform,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
+
+import {
+  Bars3CenterLeftIcon,
+  MagnifyingGlassIcon,
+} from 'react-native-heroicons/outline';
+
+const iOS = Platform.OS === 'ios';
 
 export default function HomeScreen() {
   return (
     <View className="flex-1 bg-neutral-800">
-      <SafeAreaView>
+      {/* Navigation Bar */}
+      <SafeAreaView className={iOS ? '-mb-2 mt-2' : 'mb-3 mt-2'}>
         <StatusBar style="light" />
+        <View className="flex-row justify-between items-center mx-4">
+          <Bars3CenterLeftIcon size={30} strokeWidth={2} color="white" />
+          <Text className="text-white text-3xl font-bold">
+            <Text className="text-yellow-400">M</Text>ovies
+          </Text>
+          <TouchableOpacity>
+            <MagnifyingGlassIcon size={30} strokeWidth={2} color="white" />
+          </TouchableOpacity>
+        </View>
       </SafeAreaView>
+
+      {/* Trending Movies Carousel */}
+      <ScrollView></ScrollView>
     </View>
   );
 }
