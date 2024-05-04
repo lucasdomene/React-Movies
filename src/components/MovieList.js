9bac21266fc9fb6cd748218045e6a10d1020fc4f
Lucas Domene <lucas.firmo@gmail.com>
2024-05-04 18:32:49 -0300

    feat: improving skeleton view for MovieList

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -24,6 +24,12 @@ export default function MovieList({
   const navigation = useNavigation();
   const [page, setPage] = useState(1);
 
+  const skeletonData = Array.from({ length: 3 }).map((_, index) => ({
+    id: index,
+    poster_path: '',
+    title: '',
+  }));
+
   useEffect(() => {
     console.log('USE EFFECT', page);
     if (onPagging) onPagging(page);
@@ -45,63 +51,43 @@ export default function MovieList({
         )}
       </View>
 
-      {isLoading ? (
-        <MovieListSkeleton />
-      ) : (
-        <FlatList
-          horizontal
-          data={data}
-          showsHorizontalScrollIndicator={false}
-          contentContainerStyle={{ paddingLeft: 15 }}
-          renderItem={({ item }) => {
-            return (
-              <TouchableWithoutFeedback
-                onPress={() => navigation.push('Movie', item)}
-              >
-                <View className="space-y-2 mr-4">
-                  <Skeleton show={isLoading}>
-                    <Image
-                      source={{ uri: image185(item.poster_path) }}
-                      className="rounded-3xl"
-                      style={{ width: width * 0.33, height: height * 0.22 }}
-                    />
-                  </Skeleton>
+      <FlatList
+        horizontal
+        data={isLoading ? skeletonData : data}
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingLeft: 15 }}
+        renderItem={({ item }) => {
+          return (
+            <TouchableWithoutFeedback
+              onPress={() => navigation.push('Movie', item)}
+            >
+              <View className="space-y-2 mr-4">
+                <Skeleton show={isLoading}>
+                  <Image
+                    source={{ uri: image185(item.poster_path) }}
+                    className="rounded-3xl"
+                    style={{ width: width * 0.33, height: height * 0.22 }}
+                  />
+                </Skeleton>
 
-                  {!isLoading && (
-                    <Text
-                      className="text-neutral-300 ml-1 text-center"
-                      style={{
-                        width: width * 0.3,
-                      }}
-                      numberOfLines={3}
-                      ellipsizeMode="tail"
-                    >
-                      {item.title}
-                    </Text>
-                  )}
-                </View>
-              </TouchableWithoutFeedback>
-            );
-          }}
-          keyExtractor={(item, index) => index}
-          onEndReachedThreshold={0.2}
-          onEndReached={() => setPage(page + 1)}
-        />
-      )}
+                <Text
+                  className="text-neutral-300 ml-1 text-center"
+                  style={{
+                    width: width * 0.3,
+                  }}
+                  numberOfLines={3}
+                  ellipsizeMode="tail"
+                >
+                  {item.title}
+                </Text>
+              </View>
+            </TouchableWithoutFeedback>
+          );
+        }}
+        keyExtractor={(item, index) => index}
+        onEndReachedThreshold={0.2}
+        onEndReached={() => setPage(page + 1)}
+      />
     </View>
-  );
-}
-
-function MovieListSkeleton() {
-  const numberOfItems = 3;
-
-  return (
-    <ScrollView horizontal contentContainerStyle={{ paddingLeft: 15 }}>
-      {Array.from({ length: numberOfItems }).map((_, index) => (
-        <View className="mr-4" key={index}>
-          <Skeleton width={width * 0.33} height={height * 0.22} radius={24} />
-        </View>
-      ))}
-    </ScrollView>
   );
 }
